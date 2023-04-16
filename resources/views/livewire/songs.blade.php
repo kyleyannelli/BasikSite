87e69cbd5165b7f1cdd440a0f53a77a53461c197
Kyle Yannelli <kyleyannelli@gmail.com>
2023-04-15 20:24:58 -0400

    added missing css for margins on songs page

--- resources/views/livewire/songs.blade.php
+++ resources/views/livewire/songs.blade.php
@@ -1,4 +1,4 @@
-<div class="text-center">
+<div class="text-center max-w-7xl mx-auto p-6 lg:p-8">
     @if(is_countable($songs))
         <div class="mt-16">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
